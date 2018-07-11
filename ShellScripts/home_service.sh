63ea15ecdff8d9a857a9aea007b551a7c3861856
amiltonwong <amiltonwong@gmail.com>
2018-07-11 16:18:50 +0800

    update

--- ShellScripts/home_service.sh
+++ ShellScripts/home_service.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(find turtlebot_gazebo)/worlds/MyWorld.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/MyWorld.world " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(find turtlebot_gazebo)/worlds/my_map.yaml 3d_sensor:=kinect" &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find turtlebot_gazebo)/worlds/my_map.yaml 3d_sensor:=kinect" &
 sleep 5
-xterm -e " rosrun rviz rviz -d $(find turtlebot_gazebo)/RvizConfig/home_service.rviz " &
+xterm -e " rosrun rviz rviz -d $(rospack find turtlebot_gazebo)/RvizConfig/home_service.rviz " &
 sleep 5
 xterm -e "rosrun add_markers add_markers " &
 sleep 5
 xterm -e "rosrun pick_objects pick_objects "
+
