0be7c436d00d72266d637e62e7182fc436704c71
amiltonwong <amiltonwong@gmail.com>
2018-07-11 16:19:54 +0800

    update

--- ShellScripts/test_navigation.sh
+++ ShellScripts/test_navigation.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(find turtlebot_gazebo)/worlds/MyWorld.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/MyWorld.world " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(find turtlebot_gazebo)/worlds/my_map.yaml 3d_sensor:=kinect" &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find turtlebot_gazebo)/worlds/my_map.yaml 3d_sensor:=kinect" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch "
+
