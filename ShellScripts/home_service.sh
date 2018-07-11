9bd710ee5064cb362a1e55af3c1636b44a2a2d1c
amiltonwong <amiltonwong@gmail.com>
2018-07-10 23:17:30 -0400

    commit

--- ShellScripts/home_service.sh
+++ ShellScripts/home_service.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/nvidia/catkin_ws/src/World/MyWorld.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(find turtlebot_gazebo)/worlds/MyWorld.world " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/nvidia/catkin_ws/src/World/my_map.yaml 3d_sensor:=kinect" &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(find turtlebot_gazebo)/worlds/my_map.yaml 3d_sensor:=kinect" &
 sleep 5
-xterm -e " rosrun rviz rviz -d /home/nvidia/catkin_ws/src/RvizConfig/home_service.rviz " &
+xterm -e " rosrun rviz rviz -d $(find turtlebot_gazebo)/RvizConfig/home_service.rviz " &
 sleep 5
 xterm -e "rosrun add_markers add_markers " &
 sleep 5
