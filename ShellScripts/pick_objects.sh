345ae52e014a4a447580e9cc388285dfd7807301
amiltonwong <amiltonwong@gmail.com>
2018-07-11 16:19:25 +0800

    update

--- ShellScripts/pick_objects.sh
+++ ShellScripts/pick_objects.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(find turtlebot_gazebo)/worlds/MyWorld.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/MyWorld.world " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(find turtlebot_gazebo)/worlds/my_map.yaml 3d_sensor:=kinect" &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find turtlebot_gazebo)/worlds/my_map.yaml 3d_sensor:=kinect" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
 xterm -e "rosrun pick_objects pick_objects "
+
